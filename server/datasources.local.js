81e14c10a89fb1593f21c353b22a4dc756e601b0
miguta kevin <migutak@gmail.com>
2020-01-25 22:37:29 +0300

    yes

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,7 +1,7 @@
 module.exports = {
     oracle: {
         connector: 'oracle',
-        hostname: process.env.DB_HOST || '127.0.0.1',
+        hostname: process.env.DB_HOST || '68.183.63.158',
         port: process.env.DB_PORT || 1521,
         user: process.env.DB_USER || 'ecol',
         password: process.env.DB_PASSWORD || 'ecol',
