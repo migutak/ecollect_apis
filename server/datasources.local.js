84e9d4782d22083fa2c22de3748f058c96c39724
miguta kevin <migutak@gmail.com>
2020-05-19 11:36:00 +0300

    tbl_ipf added

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -5,6 +5,6 @@ module.exports = {
         port: process.env.DB_PORT || 1521, //,
         user: process.env.DB_USER || 'ecol',
         password: process.env.DB_PASSWORD || 'ecol',//'DsQSnttm_1',
-        database: process.env.DB_DATABASE || 'ORCLCDB.localdomain'//'',
+        database: process.env.DB_DATABASE || 'ORCLCDB.localdomain'// '',
     }
 }
