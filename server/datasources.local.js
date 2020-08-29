2dd3b092de5dd3a868d6d4859f31d4e77c5e4db6
miguta kevin <migutak@gmail.com>
2020-08-29 21:37:58 +0300

    removed mysql

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -6,13 +6,5 @@ module.exports = {
         user: process.env.DB_USER || 'ecol',
         password: process.env.DB_PASSWORD || 'ecol',//'DsQSnttm_1',
         database: process.env.DB_DATABASE || 'ORCLCDB.localdomain'// 'ECOLTST',
-    },
-    mysql: {
-        connector: 'mysql',
-        hostname: process.env.MYSQL_HOST || '127.0.0.1',
-        port: process.env.MYSQL_PORT || 3305, //,
-        user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASSWORD || 'root',
-        database: process.env.MYSQL_DATABASE || 'wf_workflow'
     }
 }
