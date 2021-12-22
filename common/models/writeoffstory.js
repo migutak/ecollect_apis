606a6bcd33442325852427aa3c596c78f350f8e8
kevin abongo <kevin.abongo@royalcyber.com>
2021-12-22 13:16:58 +0300

    writeoff

--- common/models/writeoffstory.js
+++ common/models/writeoffstory.js
@@ -4,7 +4,7 @@ module.exports = function(Writeoffstory) {
     Writeoffstory.export = function (cb) {
         var ds = Writeoffstory.dataSource;
         //
-        var sql = "Select w.*,t.client_name, t.rrocode,t.arocode,t.branchname from writeoffstory w left join tqall t on t.accnumber = w.accnumber";
+        var sql = "Select w.*,t.client_name, t.rrocode,t.arocode,t.branchname,t.region,t.oustbalance from writeoffstory w left join tqall t on t.accnumber = w.accnumber";
         ds.connector.query(sql, [], function (err, accounts) {
             if (err) console.error(err);
             cb(err, accounts);
